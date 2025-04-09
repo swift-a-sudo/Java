267ffae13d8bc213b0bce8de52c2c7d49b7c486f
swift-a-sudo <amoghajoshi2005@gmail.com>
2025-04-09 14:35:56 +0530

    Save to master

--- Addition.java
+++ Addition.java
@@ -10,8 +10,11 @@ public class Addition{
         System.out.print("\nEnter the second number: ");
         int num2=scanner.nextInt(); 
 
-        int sum=num1+num2;
-        System.out.println("The sum of " +num1+ " and " +num2+ " is: "+sum);
+        System.out.print("\nEnter the third number: ");
+         int num3=scanner.nextInt(); 
+
+        int sum=num1+num2+num3;
+        System.out.println("The sum of " +num1+ " " +num2+ " and "+num3+ " is"+sum);
 
         scanner.close();
     }
